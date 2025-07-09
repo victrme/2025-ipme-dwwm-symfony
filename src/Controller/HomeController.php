35e8b716d0c29a1c3115eb8e3ef4f9bd15188bf4
Kévin Tourret <kevin@drosalys.fr>
2025-07-09 10:18:49 +0200

    entities OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Country;
 use App\Repository\CountryRepository;
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,11 +13,25 @@ final class HomeController extends AbstractController
 {
 
     #[Route(name: 'app_home')]
-    public function index(CountryRepository $countryRepository): Response
+    public function index(
+        CountryRepository   $countryRepository,
+        GameRepository      $gameRepository
+    ): Response
     {
-        /** @var Country[] $countries */
-        // SELECT * FROM country;
-//        $countries = $countryRepository->findAll();
+        /** @var Country $country */
+        // SELECT * FROM country WHERE code = 'fr';
+        $country = $countryRepository->findOneBy(
+            ['code' => 'fr']
+        );
+
+        if (null !== $country) {
+            $games = $gameRepository->findBy(
+                [],
+                ['name' => 'ASC', 'publishedAt' => 'DESC']
+            );
+            dump($games);
+        }
+//        $games =
 //        foreach ($countries as $country) {
 //            foreach ($country->getGames() as $game) {
 //                dump($country->getName(), $game->getName());
