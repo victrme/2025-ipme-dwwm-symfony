2995f44ba1a7dc31b742479bbbbcab22ae290986
Kévin Tourret <kevin@drosalys.fr>
2025-07-30 09:26:47 +0200

    route param facultatif + form with options

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,26 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegisterType;
+use App\Form\UserType;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register', methods: ['POST', 'GET'])]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(
+        Request $request,
+        EntityManagerInterface $em,
+        PasswordHasherInterface $passwordHasher,
+    ): Response
     {
         $user = new User();
-        // RegisterType::class => App\Form\RegisterType
-        $form = $this->createForm(RegisterType::class, $user);
+        // UserType::class => App\Form\UserType
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request); // Lie la requête HTTP en POST (à la soumission du form) à notre objet form
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setCreatedAt(new DateTimeImmutable());
+            $user->setPassword($passwordHasher->hash($user->getPassword()));
             $em->persist($user); // Indiquer que l'instance $user de la classe User, doit être "INSERT" en base de données
             $em->flush(); // Envoyer / confirmer que la requête d'INSERT doit passer en base de données
 
---
+++ src/Controller/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/profil/{name?}')]
+    public function index(
+        UserRepository $userRepository,
+        Request $request,
+        ?string $name
+    ): Response
+    {
+        $user = $this->getUser();
+        if ($name === null && $user === null) {
+            $this->addFlash('warning', 'Une erreur est survenue pour l affichage de ce profil');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $form = null;
+
+        if ($name !== null && $user === null) { // Lorsque je clique sur le compte d'un AUTRE utilisateur
+            $user = $userRepository->findOneBy(['name' => $name]);
+        } else { // Lorsque je clique sur MON COMPTE
+            $form = $this->createForm(UserType::class, $user, [
+                'isRegistered' => false,
+            ]);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // Oui
+            }
+        }
+
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+}
