d4916915df0b866aed7f3f0a1a23659aea90d82b
Kévin Tourret <kevin@drosalys.fr>
2025-09-18 14:04:13 +0200

    remove use

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RegisterController extends AbstractController
